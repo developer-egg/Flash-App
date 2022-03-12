0d3b6e0590a3ad61f58bd515ee00fae45a0dff09
egg <eggboissaye@gmail.com>
2022-03-12 16:04:08 -0500

    Finished home page

--- frontend/src/components/pages/HomePage.js
+++ frontend/src/components/pages/HomePage.js
@@ -1,13 +1,29 @@
-import {Container, Navbar} from "react-bootstrap"
+import {Button, Container, Navbar} from "react-bootstrap"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+
+import logo from "../.././logo.svg"
+import "../../css/home-page-styles.css"
 
 const HomePage = () => {
     return (
         <>
             <Navbar bg="dark" variant="dark" expand="lg">
                 <Container>
-                    <Navbar.Brand>Flash App</Navbar.Brand>
+                    <Navbar.Brand className="home-page-navbar-brand">
+                        <img
+                            alt=""
+                            src={logo}
+                            width="30"  
+                            height="30"
+                            className="d-inline-block home-page-navbar-brand-logo"
+                        />
+                        Flash App
+                    </Navbar.Brand>
                 </Container>
             </Navbar>
+
+            <h2 className="home-page-no-set-message">No flash card sets have been created.</h2>
+            <Button variant="outline-dark" className="home-page-create-set-button">Create Set</Button>
         </>
     )
 }
